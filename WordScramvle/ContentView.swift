85adbf9091241df627e58675d96346e83e7d8695
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2020-12-08 09:53:24 +0700

    Initial Commit

---
+++ WordScramvle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramvle
+//
+//  Created by Teneocto on 12/8/20.
+//  Copyright © 2020 NguyenCaoThiem. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
